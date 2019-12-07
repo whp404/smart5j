1c7139cea2f86aec6baea0ac866600599d91c06f
whp404 <whp404@sina.com>
2019-12-07 09:37:16 +0800

    add chapter 2

---
+++ src/main/java/com/smart5j/controller/CustomerServlet.java
@@ -0,0 +1,34 @@
+package com.smart5j.controller;
+
+import com.smart5j.model.Customer;
+import com.smart5j.service.CustomerService;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * 进入 客户列表 界面
+ */
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet {
+
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("/WEB-INF/view/customer.jsp").forward(req, resp);
+    }
+}
